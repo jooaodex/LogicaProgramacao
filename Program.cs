1c055981c395f8241295e3d5d3d680615b04e23b
jooaodex <joao.lucas1510@hotmail.com>
2022-09-29 16:20:19 -0300

    desafio2

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace desafio1
+namespace desafioss
 {
-    class Venda
+    class desafios
     {
         static void Main (string[] args)
         {
-            double id1, id2, quantidade1, quantidade2, valor1, valor2, valorProduto1, valorProduto2, valorTotal;
+           /* Double id1, id2, quantidade1, quantidade2, valor1, valor2, valorProduto1, valorProduto2, valorTotal;
 
             Console.WriteLine("Digite o ID do Prieiro Produto");
             id1 = int.Parse(Console.ReadLine());
@@ -36,8 +36,27 @@ namespace desafio1
             valorTotal = valorProduto1 + valorProduto2;
 
 
-            Console.WriteLine("Valor a pagar: " +valorTotal);
+            Console.WriteLine("Valor a pagar: " + valorTotal); */
+
+
+            // Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro casas decimais // 
+
+
+            Double raio, area, pi;   
             
+
+            Console.WriteLine("Digite o raio do circulo");
+            raio = Double.Parse(Console.ReadLine());
+
+            pi = 3.14159;
+            area = pi * (raio* raio);
+
+            
+
+            Console.WriteLine(area);
+
+            Console.ReadLine();
+
             
             
 
@@ -45,4 +64,6 @@ namespace desafio1
         }
 
     }
+
+    
 }
